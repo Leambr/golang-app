6bb09efec27799d0d1168da26e7191f73e887887
Léa Mhoumadi <mhoumadi.lea@gmail.com>
2024-02-02 16:11:50 +0100

    feat: add landing page

--- src/core/routing/routes.tsx
+++ src/core/routing/routes.tsx
@@ -1,25 +1,26 @@
-import { createBrowserRouter } from "react-router-dom";
-import SignIn from "../../components/SignIn/SingIn";
-import SignUp from "../../components/SignUp/SignUp";
-import { Layout } from "../../layout/layout";
+import { createBrowserRouter } from 'react-router-dom';
+import SignIn from '../../components/SignIn/SingIn';
+import SignUp from '../../components/SignUp/SignUp';
+import { Layout } from '../../layout/layout';
+import LandingPage from '../../pages/LandingPage/LandingPage';
 
 export const routes = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      // {
-      //   path: "/",
-      //   element: <SignIn />, // faire une homepage plutôt qui amène sur sign-in hairdresser ou customer ?
-      // },
-      {
-        path: "/sign-in/:userType",
-        element: <SignIn />,
-      },
-      {
-        path: "/sign-up/:userType",
-        element: <SignUp />,
-      }
-    ],
-  },
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                path: '/',
+                element: <LandingPage />,
+            },
+            {
+                path: '/sign-in/:userType',
+                element: <SignIn />,
+            },
+            {
+                path: '/sign-up/:userType',
+                element: <SignUp />,
+            },
+        ],
+    },
 ]);
