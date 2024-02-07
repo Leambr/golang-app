9dc1c78e0e0b4a8d395f52d8248689085372c55f
Léa Mhoumadi <mhoumadi.lea@gmail.com>
2024-02-07 11:25:48 +0100

    refactor: fix landing page name error

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -37,7 +37,7 @@ const data = [
     },
 ];
 
-export default function Pricing() {
+export default function LandingPage() {
     return (
         <>
             <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
